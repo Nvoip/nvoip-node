16df551dcbe03e43fb78aaa5e17e1064f7a4eb05
Leandro Campos <40430440+leandronvoip@users.noreply.github.com>
2026-04-29 18:09:42 -0300

    Expand Node examples for auth, OTP, calls and WhatsApp

---
+++ examples/list-whatsapp-templates.js
@@ -0,0 +1,16 @@
+import { NvoipClient } from "../src/client.js";
+
+const client = new NvoipClient({
+  baseUrl: process.env.NVOIP_BASE_URL,
+});
+
+const oauth = await client.createAccessToken({
+  numbersip: process.env.NVOIP_NUMBERSIP,
+  userToken: process.env.NVOIP_USER_TOKEN,
+});
+
+const response = await client.listWhatsAppTemplates({
+  accessToken: oauth.access_token,
+});
+
+console.log(JSON.stringify(response, null, 2));
